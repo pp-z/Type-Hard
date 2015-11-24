131d8689a0170cb1427defa08abe10c614ba7c4f
Raheem <abduraheempp@gmail.com>
2015-11-24 21:47:38 +0530

    Game Over

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 $(document).ready(function($){
 	var y=10;
+	var speed=2;
 	function render()
 	{
 
 	$("#word").css({"top":y+"px"});
-	y+=10;
-	setTimeout(render,100)
+	y+=speed;
+	setTimeout(render,10)
+    
+
+    if(y>500)
+    {
+    	$("#end").html("Game Over");
+    	$("#word").empty();
     }
-
-
+}
 	window.addEventListener("load", function(){ 
 		render();
-	});
+		});
+	
 });
